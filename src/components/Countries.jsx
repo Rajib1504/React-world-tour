04b78efef2215450b669e973ed24b283acb8602c
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-20 12:57:18 +0530

    style

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -3,25 +3,25 @@ import { useState } from "react";
 import Country from "./Country/Country";
 
 const Countries = () => {
-      const [countries, setCountries] = useState([])
-      useEffect(()=>{
-            fetch('https://restcountries.com/v3.1/all')
-            .then(res => res.json())
-            .then (data => setCountries(data))
-      },[])
-      return (
-            <div>
-                 <h3>Countries:{countries.length}</h3> 
-                 {
-                 countries.map(country =><Country key={country.cca2} country ={country}></Country>)
-                 }
-            </div>
-      );
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
+    <div>
+      <h3>Countries:{countries.length}</h3>
+      {countries.map((country) => (
+        <Country country={country} key={country.cca3}></Country>
+      ))}
+    </div>
+  );
 };
 
 export default Countries;
 // rsc extention
 
 // for api:
-// 1.for data store create a state 
-// 2. fetach the data by using the useEffect 
+// 1.for data store create a state
+// 2. fetach the data by using the useEffect
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,17 @@
-import './country.css';
-const Country = ({country}) => {
-      // console.log(country);
-      const { name } = country;
-      const {flags } =country;
-      return (
-            <div className="box">
-                  <img src={flags.png}alt=""/>
-                  <h3>Name:{name?.common}</h3>
-
-            </div>
-      );
+import "./Country.css";
+const Country = ({ country }) => {
+  console.log(country);
+  const { name, flags, population, area } = country;
+  return (
+    <div className="box">
+      <img src={flags.png} alt="" />
+      <div className="flex">
+        <h3 className="border">Name:{name.common}</h3>
+        <p className="border">Population:{population}</p>
+        <p className="border">area:{area}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Country;
